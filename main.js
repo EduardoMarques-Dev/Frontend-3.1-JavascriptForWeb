662976f4353806ce2951349c3da30813c840d677
Carlos Eduardo <carlospereira@pactosolucoes.com.br>
2022-08-23 20:27:29 -0300

    A04 Iterando em listas

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+const ListaDeTeclas = document.querySelectorAll('.tecla');
+let contador = 0;
+
+while (contador < ListaDeTeclas.length) {
+
+    const tecla = ListaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    // template string
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    };
+
+    contador ++;
+}
